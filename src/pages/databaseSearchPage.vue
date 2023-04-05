0e9e743208a3d2b85391cc45ae3b561c361c05e6
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-05 14:04:05 +0200

    enables reading typesense config from .env file

--- src/pages/databaseSearchPage.vue
+++ src/pages/databaseSearchPage.vue
@@ -157,12 +157,9 @@ const typesenseInstantSearchAdapter = new TypesenseInstantSearchAdapter({
     apiKey: import.meta.env.VITE_TYPESENSE_API_KEY,
     nodes: [
       {
-        // host: import.meta.env.VITE_TYPESENSE_HOST,
-        // port: import.meta.env.VITE_TYPESENSE_PORT,
-        // protocol: import.meta.env.VITE_TYPESENSE_PROTOCOL,
-        host: "localhost",
-        port: 8109,
-        protocol: "http",
+        host: import.meta.env.VITE_TYPESENSE_HOST,
+        port: import.meta.env.VITE_TYPESENSE_PORT,
+        protocol: import.meta.env.VITE_TYPESENSE_PROTOCOL,
       },
     ],
   },
