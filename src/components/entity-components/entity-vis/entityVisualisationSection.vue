0035bfb5c1a60500dc21be5b0cab1a3957b0d711
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-05 11:51:01 +0200

    Removes unecessary define props import.

--- src/components/entity-components/entity-vis/entityVisualisationSection.vue
+++ src/components/entity-components/entity-vis/entityVisualisationSection.vue
@@ -1,13 +1,12 @@
-
-
 <template>
-    <div class="bg-gray-200 rounded text-white h-20"> Visualisation Section {{ props.ent_type }}</div>
+  <div class="bg-gray-200 rounded text-white h-20">
+    Visualisation Section {{ props.ent_type }}
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 
-import { defineProps, ref } from "vue";
-
-const props = defineProps({ ent_type: String })
+const props = defineProps({ ent_type: String });
 </script>
 <style scoped></style>
