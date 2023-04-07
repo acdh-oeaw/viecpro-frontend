11667d207ee8fb023db88212f4b166f56f5c2113
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-07 14:10:42 +0200

    removed texts file imports

--- src/pages/landingPage.vue
+++ src/pages/landingPage.vue
@@ -3,19 +3,14 @@ via the home navlink in the main navbar and by clicking on the viecpro logo in
 the main navbar
 <script setup lang="ts">
 import { onBeforeMount, ref, watch } from "vue";
-import { texts } from "@/texts.js";
 
 //section components
 import welcomeSection from "./landing-page-sections/welcomeSection.vue";
 import databaseSection from "./landing-page-sections/databaseSection.vue";
 import contactSection from "./landing-page-sections/contactSection.vue";
 import documentationSection from "./landing-page-sections/documentationSection.vue";
-const textFile = texts;
 
-watch(textFile, () => {
-  // for debuggin now only
-  console.log("textfiel content: ", textFile);
-});
+
 </script>
 
 <template>
