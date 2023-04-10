6238a431ce9d88618ae3cc5eee2abe3d18f1150f
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-10 15:40:36 +0200

    feat: cleans generic table and implements redirect to related entity in relations collection results

--- src/components/search-components/results/genericResultsTable.vue
+++ src/components/search-components/results/genericResultsTable.vue
@@ -4,7 +4,6 @@ import useOpenDetail from '../../../composables/useOpenDetail.ts';
 import useOpenDetailNew from '../../../composables/useOpenDetailNew.ts';
 const props = defineProps(['headers', 'items', 'tB']);
 
-
 //const tB = props.model == "Person"? "search-result-table-headers.person" : "search-result-table-headers."
 // onMounted(() => {
 //   watch(props.headers, () => {
@@ -17,7 +16,7 @@ const props = defineProps(['headers', 'items', 'tB']);
     <table>
       <tr>
         <th v-for="header in headers" :key="header" class="text-start capitalize pr-8 border-b-2">
-          {{ model ? 'Person' : $t(tB + header) }}
+          {{ $t(tB + header) }}
         </th>
       </tr>
       <tr
@@ -29,11 +28,28 @@ const props = defineProps(['headers', 'items', 'tB']);
       >
         <td v-for="header in headers" :key="header" class="text-sm py-1 pr-4">
           <ais-highlight
+            v-if="['name', 'first_name'].includes(header)"
             :hit="item"
             :attribute="header"
             class="entity_tag hover:cursor-pointer hover:underline"
             @click="useOpenDetailNew(item.model, item.object_id)"
           ></ais-highlight>
+          <ais-highlight
+            v-else-if="header == 'target.name'"
+            :hit="item"
+            :attribute="header"
+            class="entity_tag hover:cursor-pointer hover:underline"
+            @click="useOpenDetailNew(item.target.model, item.target.object_id)"
+          ></ais-highlight>
+          <ais-highlight
+            v-else-if="header == 'source.name'"
+            :hit="item"
+            :attribute="header"
+            class="entity_tag hover:cursor-pointer hover:underline"
+            @click="useOpenDetailNew(item.source.model, item.source.object_id)"
+          ></ais-highlight>
+          <ais-highlight v-else :hit="item" :attribute="header" class="entity_tag"></ais-highlight>
+
           <!--                     @click="redirectToEntity('entities', item.id, item.model)"
  -->
         </td>
