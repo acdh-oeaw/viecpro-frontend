1c24da327ce85b18f56312046fdd4869e12a5c5c
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-11 12:15:28 +0200

    feat: adds new generic composable
    - composable implements grouping array of objects by a given key.

---
+++ src/composables/transform-data/useGroupArrayOfObjectsByKey.ts
@@ -0,0 +1,15 @@
+export default function useGroupArrayOfObjectsByKey(data, key) {
+
+    // TODO: type this
+    const temp: object = {};
+  
+    data.forEach((el) => {
+      if (Object.keys(temp).includes(el[key])) {
+        temp[el[key]].push(el);
+      } else {
+        temp[el[key]] = [el];
+      }
+    });
+    return temp;
+  }
+  
