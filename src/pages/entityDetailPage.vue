ebb771522b64906b1b03b654f18ccb30df3c6b56
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-13 12:29:34 +0200

    code: cleanup

--- src/pages/entityDetailPage.vue
+++ src/pages/entityDetailPage.vue
@@ -1,8 +1,3 @@
-// Generic detail page for all entities // maped to a dynamic route // generic sections are coded as
-external components /* TODO: at the moment, this same view deals with entities and relations display
-(i.e. you can view a single relation instance as the base entity. This needs to be split into two
-different generic components - if we want to show detail views of relations at all.) */
-
 <script setup lang="ts">
 // utitily imports
 import { ref, onBeforeMount, computed, watch, onMounted } from 'vue';
@@ -26,7 +21,6 @@ const props = defineProps({
 
 const data: Ref<object> = ref({}); // TODO: make more clear distinction between metada and relations-data
 const relations: Ref<object> = ref({});
-const showInformation: Ref<boolean> = ref(false); // TODO: get rid of this
 const collection = useGetCollectionFromModel(props.model);
 const doc_id = useConstructDocIDFromParams(props.model, props.object_id);
 const labels = ref({});
@@ -36,27 +30,7 @@ useTypesenseAsyncRetrieval(collection.value, doc_id, (response) => {
 });
 
 // TODO: make the two data fetching functions failsafe and work together.
-// onBeforeMount(() => {
-//     function process_results(response) {
-//     const docs = useExtractHitsFromResults(response);
-//     const transformedRelations = useGroupRelationsByClass(docs);
-//     labels.value = useGroupArrayOfObjectsByKey(data.value.labels, "label_type")
-//     // TODO: consider moving grouping logic into meta views of specific classes (Person, Institution)
 
-//     data.value["grouped_labels"] = labels.value
-//     data.value['relations'] = transformedRelations;
-//     relations.value = transformedRelations;
-//   }
-
-//   useTypesenseAsyncQuery(
-//     'Relations',
-//     props.object_id,
-//     'source.object_id, target.object_id',
-//     process_results
-//     // { filter_by: '', sort_by: '', per_page: 200, num_typos: 0 },
-//   );
-// });
-//onMounted(() => {
 watch(data, () => {
   // TODO: this is a hack atm, settings value below should re-trigger the watcher... improve this.
   function process_results(response) {
@@ -78,7 +52,7 @@ watch(data, () => {
     // { filter_by: '', sort_by: '', per_page: 200, num_typos: 0 },
   );
 });
-//});
+
 </script>
 <template>
   <div id="main-container flex" class="bg-white">
@@ -116,12 +90,7 @@ watch(data, () => {
           <!-- TODO: implement correct loading logic for all components in a reusable fashion -->
           <TabPanel class="tab-panel-standard">
             {{ data ? data : 'loading' }}
-            <div
-              class="bg-black text-white w-20 h-20 pt-7 text-center"
-              :class="showInformation ? 'visible' : 'hidden'"
-            >
-              I
-            </div>
+      
           </TabPanel>
           <TabPanel class="tab-panel-standard">
             <div v-for="(values, model) in relations" :key="model + '_relation'">
