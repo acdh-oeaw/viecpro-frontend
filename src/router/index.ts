828e54efcba4814ec3cfdde2d6bf4b7deccf6ff1
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-14 09:23:20 +0200

    fix: uppercase component names in Routes; adds SearchPageNew component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,37 @@
-import { createRouter, createWebHistory } from "vue-router";
-import landingPage from "../pages/LandingPage.vue";
-import databaseSearchPage from "../pages/SearchPage.vue";
-import entityDetailPage from "@/pages/EntityDetailPage.vue";
-import deduplicationPage from "@/pages/DeduplicationPage.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import LandingPage from '../pages/LandingPage.vue';
+import SearchPage from '../pages/SearchPage.vue';
+import SearchPageNew from '../pages/SearchPageNew.vue';
+import EntityDetailPage from '@/pages/EntityDetailPage.vue';
+import DeduplicationPage from '@/pages/DeduplicationPage.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: landingPage,
+      path: '/',
+      name: 'home',
+      component: LandingPage,
     },
     {
-      path: "/about",
-      name: "about",
+      path: '/about',
+      name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../pages/AboutPage.vue"),
+      component: () => import('../pages/AboutPage.vue'),
     },
-    { path: "/search", name: "db-search", component: databaseSearchPage },
+    { path: '/search', name: 'db-search', component: SearchPage },
     {
-      path: "/detail/:model/:object_id/",
-      component: entityDetailPage,
+      path: '/detail/:model/:object_id/',
+      component: EntityDetailPage,
       props: true,
     },
-    {
-      path: "/deduplication",
-      name: "Deduplication",
-      component: deduplicationPage,
-    },
+    { path: '/search/:collection/', name: 'search_new', component: SearchPageNew, props = true },
+    // {
+    //   path: "/deduplication",
+    //   name: "Deduplication",
+    //   component: DeduplicationPage,
+    // },
   ],
 });
 
