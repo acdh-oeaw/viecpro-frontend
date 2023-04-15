d5f8d957c76d5161b818c66dfc7b42321450a5ec
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-15 14:21:22 +0200

    re adds correct file

---
+++ src/components/entity-components/entity-vis/EntityVisualisationSection.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-gray-200 rounded text-white h-20">
+    Visualisation Section {{ props.ent_type }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps({ ent_type: String });
+</script>
+<style scoped></style>
