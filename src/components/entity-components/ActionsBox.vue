df9be59a8fdac85a532b679bf735acb67e3433dc
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-16 13:21:30 +0200

    adds configuration for push to main  and tests dummy download

--- src/components/entity-components/ActionsBox.vue
+++ src/components/entity-components/ActionsBox.vue
@@ -7,7 +7,8 @@ useTypesenseAsyncRetrieval("Reference", 'Reference_95007', log_res)
 </script>
 <template>
   <div class="w-80 h-40 border-2 flex-col place-content-between bg-gray-200">
-    <div class="mb-4"><p>Donwnload</p></div>
+    <div class="mb-4"><a href="/src/assets/logos/logo_white.png" download="test_logo.png">Donwnload</a></div>
+
     <div>Zitation</div>
     <div>Visualisation</div>
   </div>
