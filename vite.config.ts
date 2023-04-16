b9f3f39b4292ef368a99b062c9a21c4ae67288c6
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-16 14:43:15 +0200

    removes unsued code

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,6 @@ import { resolve, dirname } from "node:path";
 
 
 
-// const viteEnv = {}
-// Object.keys(process.env).forEach((key) => {
-//   if (key.startsWith(`VITE_`)) {
-//     viteEnv[`import.meta.env.${key}`] = process.env[key]
-//   }
-// })
-// https://vitejs.dev/config/
 
 export default defineConfig({
   plugins: [
