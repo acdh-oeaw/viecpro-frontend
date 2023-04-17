34d2c17ab25bb98aaf8321273a2943681de87f2d
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-17 17:41:35 +0200

    responsive layout for detail page

--- src/pages/GenericDetailPage.vue
+++ src/pages/GenericDetailPage.vue
@@ -9,13 +9,13 @@
   <!-- GENERAL LAYOUT -->
   <div id="container-main" class="flex-col text-center min-h-screen bg-gray-200">
     <div id="container-above-split" class="bg-yellow-100">Above Container</div>
-    <div id="container-split" class="bg-red-50 p-10 md:justify-between flex-col md:flex">
-      <div id="container-split-left" class="flex-col w-1/4 bg-red-200">
-        <div id="container-meta" class="bg-gray-300 mb-10 h-60">Meta Section</div>
-        <div id="container-below-meta" class="bg-gray-300 h-40 mb-10">Below Meta</div>
+    <div id="container-split" class="bg-red-50 p-10 xl:justify-between flex flex-col xl:flex-row">
+      <div id="container-split-left" class="flex-col w-full xl:w-1/4 bg-red-200 xl:mb-0 mb-10">
+        <div id="container-meta" class="bg-gray-300 mb-10 h-60 w-full">Meta Section</div>
+        <div id="container-below-meta" class="bg-gray-300 h-40 mb-10 w-full">Below Meta</div>
       </div>
-      <div id="container-split-right" class="flex-col w-2/3 bg-red-200">
-        <div id="container-relations" class="bg-gray-300 mb-10 h-60">Relations</div>
+      <div id="container-split-right" class="flex-col xl:w-2/3 bg-red-200  xl:mb-0 mb-10">
+        <div id="container-relations" class="bg-gray-300 mb-10 h-60 w-full">Relations</div>
         <div id="container-below-relations" class="bg-gray-300 h-40 mb-10">Below Relations</div>
       </div>
     </div>
