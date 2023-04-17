c04795d256a674be732d41b868d11e0616a9a2de
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-17 13:13:17 +0200

    layout for new pages

---
+++ src/pages/GenericDetailPage.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+// define props as entry point
+
+
+// fetch additional data
+
+
+// prepare data for all sub-views (name them accordingly)
+
+
+
+</script>
+<template>
+
+    <!-- GENERAL LAYOUT -->
+    <div id="container-main" class="flex-col">
+    <div id="container-above"></div>
+    <div id="container-split">
+    <div id="container-split-left"></div>
+    <div id="container-split-right"></div>
+    
+    </div>
+    
+    </div>
+
+
+
+</template>
+
+<style scoped>
+</style>
