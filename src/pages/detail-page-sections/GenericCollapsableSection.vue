b22a030fcf33217169348ba16acc822d0f74b9f0
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-19 13:13:31 +0200

    adds fontawesome icons to app

--- src/pages/detail-page-sections/GenericCollapsableSection.vue
+++ src/pages/detail-page-sections/GenericCollapsableSection.vue
@@ -5,7 +5,7 @@ const props = defineProps(['header', 'data', 'isCollapsed']);
   <div class="border-2 rounded px-10">
     <div class="flex justify-between">
       <h2
-      class="my-auto py-5"
+        class="my-auto py-5"
         :class="{
           'text-gray-400 cursor-not-allowed': !data.length,
           'hover:cursor-pointer': data.length,
@@ -26,15 +26,14 @@ const props = defineProps(['header', 'data', 'isCollapsed']);
           }
         "
       >
-        Toggle
+        <i v-if="isCollapsed" class="fas fa-sm fa-arrow-down text-gray-400"></i>
+        <i v-else class="fas fa-sm fa-arrow-up text-gray-400"></i>
       </button>
       <!-- <p v-else class="box text-gray-400 font-mono my-auto py-5">KEINE DATEN</p> -->
-
     </div>
-      <div :class="{ hidden: isCollapsed }" class="flex-col">
-        <slot></slot>
-
-      </div>
+    <div :class="{ hidden: isCollapsed }" class="flex-col">
+      <slot></slot>
+    </div>
   </div>
 </template>
-<style scoped></style>
+<style></style>
