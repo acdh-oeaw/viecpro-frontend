9340a09f372f81570af55727e1b0c4028060b7d3
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-21 04:30:21 +0200

    adds branches for all citations

--- src/composables/utils/useConstructCitation.ts
+++ src/composables/utils/useConstructCitation.ts
@@ -1,12 +1,33 @@
+export default function useConstructCitation(model: string, metaData: object) {
+  console.log('construct citation called with', model, metaData);
+  const date = new Date();
+  // TODO: add correctly fomatted date here
 
+  let citation: string;
 
-export default function useConstructCitation(model:string, metaData:object){
+  switch (model) {
+    case 'Person':
+      if (metaData.gender == 'female') {
+        // TODO: add verheirateten namen
+        citation = `${model}: ${metaData.name}, ${metaData.first_name} (ID: ${metaData.object_id}), in: VieCPro, hrsg. vom Institute for Habsburg and Balkan Studies der ÖAW, Wien. URL: 'https://viecpro.oeaw.ac.at/detail/${model}/${metaData.object_id}/'. (abgerufen am: DATUM)`;
+      } else {
+        citation = `${model}: ${metaData.name}, ${metaData.first_name} (ID: ${metaData.object_id}), in: VieCPro, hrsg. vom Institute for Habsburg and Balkan Studies der ÖAW, Wien. URL: 'https://viecpro.oeaw.ac.at/detail/${model}/${metaData.object_id}/'. (abgerufen am: DATUM)`;
+      }
+      break;
+    case 'Institution':
+      citation = `${model}: ${metaData.name} (ID: ${metaData.object_id}), in: VieCPro, hrsg. vom Institute for Habsburg and Balkan Studies der ÖAW, Wien. URL: 'https://viecpro.oeaw.ac.at/detail/${model}/${metaData.object_id}/'. (abgerufen am: DATUM)`;
 
-    console.log("construct citation called with", model, metaData)
-    let date = new Date();
-    // TODO: add correctly fomatted date here
-const citation = `${model}: ${metaData.name}, ${metaData.first_name} (ID: ${metaData.object_id}), in: VieCPro, hrsg. vom Institute for Habsburg and Balkan Studies der ÖAW, Wien. URL: 'https://viecpro.oeaw.ac.at/detail/${model}/${metaData.object_id}/'. (abgerufen am: DATUM)`
+      break;
+    case 'Place':
+      citation = `${model}: ${metaData.name} (ID: ${metaData.object_id}), in: VieCPro, hrsg. vom Institute for Habsburg and Balkan Studies der ÖAW, Wien. URL: 'https://viecpro.oeaw.ac.at/detail/${model}/${metaData.object_id}/'. (abgerufen am: DATUM)`;
 
-console.log(citation)
-return citation
+      break;
+    default:
+      console.log('reached default in get citation ');
+      citation = `${model}: ${metaData.name} (ID: ${metaData.object_id}), in: VieCPro, hrsg. vom Institute for Habsburg and Balkan Studies der ÖAW, Wien. URL: 'https://viecpro.oeaw.ac.at/detail/${model}/${metaData.object_id}/'. (abgerufen am: DATUM)`;
+      break;
+  }
+
+  console.log(citation);
+  return citation;
 }
