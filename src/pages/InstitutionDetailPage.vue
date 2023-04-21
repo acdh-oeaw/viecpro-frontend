4f7a8aa4963d62f48a963f0d795d72750e9ed191
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-21 05:36:37 +0200

    fix: re-adds laufzeit to institutiond detail

--- src/pages/InstitutionDetailPage.vue
+++ src/pages/InstitutionDetailPage.vue
@@ -207,6 +207,11 @@ watch(rawDocData, () => {
                   <span v-if="labelData.kategorie"> {{ labelData.kategorie }} </span>
                   <span v-else> - </span>
                 </p>
+                <label class="col-span-1" for=""> Laufzeit: </label>
+                <p class="col-span-3">
+                   <span v-if="metaData.start_date"> {{ metaData.start_date }}</span> <span v-else> {{'?'}}</span>  <span> <span>{{'-'}} </span></span><span v-if="metaData.end_date"> {{ metaData.end_date }} </span>
+<span v-else> ? </span>
+                </p>
                 <!--
                 <label class="col-span-1" for=""> Auflösung: </label>
                 <p class="col-span-3">
